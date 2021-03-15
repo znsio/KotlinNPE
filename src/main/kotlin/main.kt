e80a5be9a49d55b15872a6a9f2f654e9fdf9006b
Julian Harty (RIL) <julian.harty@ril.com>
2021-03-15 13:23:08 +0000

    Code to reproduce the NPE

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var tinyMe = MyParticipant()
+    var littleMe = MyParticipant(isCoHost = false)
+    var middleMe = MyParticipant(isHost = false, isCoHost = true)
+    var bigMe = MyParticipant(isHost = true, isCoHost = true)
+
+    bindAsNotAnOwner(bigMe)
+    bindAsNotAnOwner(middleMe)
+    bindAsNotAnOwner(littleMe)
+    bindAsNotAnOwner(tinyMe)
+}
+
+data class MyParticipant(
+    @JvmField var isHost: Boolean = false,
+    @JvmField var isCoHost: Boolean? = null
+)
+
+fun bindAsNotAnOwner(participant: MyParticipant) {
+    when {
+        participant.isHost -> {
+            println("I'm a host")
+        }
+        participant.isCoHost!! -> {
+            println("I'm a co-host")
+        }
+        else -> {
+            println("Who am I?")
+        }
+    }
+}
